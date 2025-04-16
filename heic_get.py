51a540c944247e27bcae1b1574daffff77dc1a61
JK <jkotoulek@gmail.com>
2025-04-16 15:56:06 -0400

    Formatting for heic_get

--- heic_get.py
+++ heic_get.py
@@ -3,6 +3,7 @@ from pillow_heif import register_heif_opener
 import argparse
 import webbrowser
 
+
 def get_exif(filename):
     image = Image.open(filename)
     image.verify()
@@ -29,38 +30,25 @@ def get_geotagging(exif):
         return geo_tagging_info
 
 
-register_heif_opener()
+if __name__ == '__main__':
+    register_heif_opener()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file-name", required=True, help="Enter .heic file here.")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file-name", required=True, help="Enter .heic file here.")
+    args = parser.parse_args()
 
-my_image = args.file_name
-image_info = get_exif(my_image)
-results = get_geotagging(image_info)
-print(results)
+    my_image = args.file_name
+    image_info = get_exif(my_image)
+    results = get_geotagging(image_info)
+    print(results)
 
-if input("Open in Google Maps? (y/*): ").lower() == 'y':
-    latitude = results["GPSLatitude"].strip('()').split(',')
-    longitude = results["GPSLongitude"].strip('()').split(',')
-    lat = float(latitude[0]) + float(latitude[1])/60 + float(latitude[2])/3600
-    lon = float(longitude[0]) + float(longitude[1])/60 + float(longitude[2])/3600
 
-    url = f"https://www.google.com/maps/search/?api=1&query={lat}{results["GPSLatitudeRef"]},{lon}{results["GPSLongitudeRef"]}"
-    webbrowser.open(url)
+    if input("Open in Google Maps? (y/*): ").lower() == 'y':
+        latitude = results["GPSLatitude"].strip('()').split(',')
+        longitude = results["GPSLongitude"].strip('()').split(',')
+        lat = float(latitude[0]) + float(latitude[1])/60 + float(latitude[2])/3600
+        lon = float(longitude[0]) + float(longitude[1])/60 + float(longitude[2])/3600
 
-# x used to mask data
-# {'GPSLatitudeRef': 'N', 
-# 'GPSLatitude': '(3x.0, 5x.0, 1x.0x)', 
-# 'GPSLongitudeRef': 'W', 
-# 'GPSLongitude': '(8x.0, 2x.0, 5x.2x)', 
-# 'GPSAltitudeRef': "b'\\x00'", 
-# 'GPSAltitude': '279.63243243243244', 
-# 'GPSSpeedRef': 'K', 
-# 'GPSSpeed': '0.04649941997239198', 
-# 'GPSImgDirectionRef': 'T', 
-# 'GPSImgDirection': '274.37165833514456', 
-# 'GPSDestBearingRef': 'T', 
-# 'GPSDestBearing': '27x.37165833514456', 
-# 'GPSDateStamp': '2022:06:12'}
+        url = f"https://www.google.com/maps/search/?api=1&query={lat}{results["GPSLatitudeRef"]},{lon}{results["GPSLongitudeRef"]}"
+        webbrowser.open(url)
 
